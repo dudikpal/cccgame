d409a98d3a53dc1080d45b1e79bb7ab0fc772ae0
Dudik Pál <dudikpal@gmail.com>
2022-06-12 00:06:30 +0200

    app prop to prod

---
+++ src/test/java/com/games/cccgame/CccgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.games.cccgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CccgameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
