0bd9cbec1ef4226583c5d410ad894c40db8fea13
Dudik Pál <dudikpal@gmail.com>
2022-06-12 20:40:33 +0200

    app prop to prod

---
+++ src/main/java/com/games/cccgame/controllers/CardController.java
@@ -0,0 +1,33 @@
+package com.bezkoder.spring.login.controllers;
+
+import com.bezkoder.spring.login.service.CardService;
+import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:4200"/*, allowedHeaders = "*", allowCredentials = "true"*/)
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/cards")
+public class CardController {
+
+    private CardService cardService;
+
+    @GetMapping
+    //@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    public String getAllCards() {
+
+        return cardService.getAllCards();
+    }
+
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String getAdminPage() {
+
+        return "<button>Admin page</button>";
+    }
+}
