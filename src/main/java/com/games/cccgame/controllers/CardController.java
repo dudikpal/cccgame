0b685e196da61af77baf269c0580a5c6ab163da1
Dudik Pál <dudikpal@gmail.com>
2022-06-14 13:11:30 +0200

    front and back in one place

--- src/main/java/com/games/cccgame/controllers/CardController.java
+++ src/main/java/com/games/cccgame/controllers/CardController.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:4200"/*, allowedHeaders = "*", allowCredentials = "true"*/)
 @AllArgsConstructor
 @RestController
-@RequestMapping
-//@RequestMapping("/api/cards")
+//@RequestMapping
+@RequestMapping("/api/cards")
 public class CardController {
 
     private CardService cardService;
