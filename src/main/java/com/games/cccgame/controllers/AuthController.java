b1f93404893d311e7c2c5d274e98a642685a2842
Dudik Pál <dudikpal@gmail.com>
2022-06-17 00:06:46 +0200

    registration save user properly

--- src/main/java/com/games/cccgame/controllers/AuthController.java
+++ src/main/java/com/games/cccgame/controllers/AuthController.java
@@ -35,7 +35,7 @@ import com.games.cccgame.security.jwt.JwtUtils;
 import com.games.cccgame.security.services.UserDetailsImpl;
 
 //@CrossOrigin(origins = "*", maxAge = 3600)
-@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", allowCredentials = "true")
+//@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", allowCredentials = "true")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
