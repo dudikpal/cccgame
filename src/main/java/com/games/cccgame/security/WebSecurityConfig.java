336e2e9549d5b767a6e81f6ad1d9f8a71e75adf8
Dudik Pál <dudikpal@gmail.com>
2022-06-18 22:15:50 +0200

    login 404

--- src/main/java/com/games/cccgame/security/WebSecurityConfig.java
+++ src/main/java/com/games/cccgame/security/WebSecurityConfig.java
@@ -59,12 +59,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.cors().and().csrf().disable()
-      .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+      /*.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()*/
       .authorizeRequests().antMatchers("/api/auth/**").permitAll()
       .antMatchers("/api/test/**").permitAll()
-      .antMatchers("/api/cards").permitAll()
-      //.antMatchers("/").permitAll()
+      .antMatchers("/api/cards").authenticated()
+      .antMatchers("/").permitAll()
       /*.anyRequest().authenticated()*/;
 
     http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
