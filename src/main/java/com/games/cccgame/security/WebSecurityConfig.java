973f8d155aabec230a9fc30c20a4bbc9e5281701
Dudik Pál <dudikpal@gmail.com>
2022-08-07 15:55:20 +0200

    permitall

--- src/main/java/com/games/cccgame/security/WebSecurityConfig.java
+++ src/main/java/com/games/cccgame/security/WebSecurityConfig.java
@@ -64,7 +64,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/test/**").permitAll()
             .antMatchers("/api/cards").permitAll()
             .antMatchers("/").permitAll()*/
-        /*.anyRequest().authenticated()*/;
+        .anyRequest().permitAll();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
